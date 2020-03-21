160fbae2ec37d1aed6eff8357eae6026cb01ad68
Aspodel D <dattran2309@gmail.com>
2020-03-21 15:22:09 +0700

    Version 1.0.6

--- src/Component/footer.jsx
+++ src/Component/footer.jsx
@@ -1,21 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { fab, faFacebookSquare, faTwitterSquare, faDiscord } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope, faPhone} from '@fortawesome/free-solid-svg-icons';
-
+import {
+  fab,
+  faFacebookSquare,
+  faTwitterSquare,
+  faDiscord
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faEnvelope,
+  faPhone,
+  faMapMarker,
+  faMapMarkerAlt
+} from "@fortawesome/free-solid-svg-icons";
 
 class Footer extends Component {
-    render() { 
-        return ( 
-            <div id="footer">
-                <a href="#"><FontAwesomeIcon icon={faEnvelope}/></a>
-                <a href="#"><FontAwesomeIcon icon={faFacebookSquare} /></a>
-                <a href="#"><FontAwesomeIcon icon={faTwitterSquare} /></a>
-                <a href="#"><FontAwesomeIcon icon={faDiscord} /></a>
-                <a href="#"><FontAwesomeIcon icon={faPhone} /></a>
-            </div>
-         );
-    }
+  render() {
+    return (
+      <div id="footer">
+        <div className="footer-left">
+          <div>
+            <FontAwesomeIcon className="icon" icon={faMapMarkerAlt} />
+            <p>Ho Chi Minh City, VN</p>
+          </div>
+          <div>
+            <FontAwesomeIcon className="icon" icon={faEnvelope} />
+            <p>Giasu@email.com</p>
+          </div>
+          <div>
+            <FontAwesomeIcon className="icon" icon={faPhone} />
+            <p>(+84) 123-456-789</p>
+          </div>
+        </div>
+
+        <div className="footer-right">
+          <div className="header" >About Us</div>
+          <div className="content">Lorem ipsum dolor sit amet, consect adipisicing elit. Illo enim maiores explicabo in eaque incidunt! Humhhem ipsum dolor, sit amet consectetur adipisicing elit. Enim, rerum.</div>
+          <div className="social-media">
+            <a href="#">
+              <FontAwesomeIcon icon={faFacebookSquare} />
+            </a>
+            <a href="#">
+              <FontAwesomeIcon icon={faTwitterSquare} />
+            </a>
+            <a href="#">
+              <FontAwesomeIcon icon={faDiscord} />
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default Footer;
